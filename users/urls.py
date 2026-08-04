182b8ec31a6289b4e3095ec99060ee7c7e453253
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-08-04 21:50:12 +0300

    make path for apps users in urls

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import UserProfileUpdateAPIView
+
+app_name = UsersConfig.name
+
+# маршрутизация путей для приложения, необходимо зарегистрировать путь приложения в основном settings/urls.py
+urlpatterns = [
+    # <int:pk> — это ID пользователя, профиль которого мы хотим посмотреть или изменить
+    path("users/<int:pk>/", UserProfileUpdateAPIView.as_view(), name="user-profile"),
+]
