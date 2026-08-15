7250fa838403ec76376db2aa897eccebb336d081
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-08-15 15:17:50 +0300

    add PaymentsSerializer and add field-'payments_history' in UserProfileSerializer from data PaymentSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,35 @@
-from rest_framework.serializers import ModelSerializer
-from users.models import User
+from rest_framework import serializers
+
+from users.models import User, Payment
 
 
-class UserProfileSerializer(ModelSerializer):
-    """Сериализатор для редактирования личных данных пользователя."""
+class PaymentSerializer(serializers.ModelSerializer):
+    """Сериализатор для вывода списка платежей с фильтрацией для эндпоинта вывода списка платежей с возможностями:
+    - менять порядок сортировки по дате оплаты
+    - фильтровать по курсу или уроку
+    - фильтровать по способу оплаты."""
+
+    class Meta:
+        model = Payment
+        fields = [
+            "id",
+            "user",
+            "paid_date",
+            "paid_course",
+            "paid_lesson",
+            "payment_amount",
+            "payment_method",
+        ]
+        # ID делаем только для чтения.
+        read_only_fields = [
+            "id",
+        ]
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Сериализатор для профиля пользователя с историей его платежей."""
+
+    payments_history = PaymentSerializer(source="payments", many=True, read_only=True)
 
     class Meta:
         model = User
@@ -15,6 +41,26 @@ class UserProfileSerializer(ModelSerializer):
             "phone_number",
             "city",
             "avatar",
+            "payments_history",
         ]
         # ID и email делаем только для чтения.
-        read_only_fields = ["id",]
+        read_only_fields = ["id", "email"]
+
+    # # Метод получения последнего платежа с использованием метода get_last_payment
+    # # last_payment в этом случае необходимо включить в поля сериализатора user
+    # last_payment = serializers.SerializerMethodField()
+    #
+    # def get_last_payment(self, instance):
+    #     # instance — это текущий User.
+    #     # ordering=['-paid_date'] в модели уже сортирует от новых к старым.
+    #     # Берем самый свежий платеж пользователя:
+    #     latest_payment = instance.payments.first()
+    #
+    #     if latest_payment:
+    #         # Возвращаем информацию в виде удобного словаря
+    #         return {
+    #             "amount": latest_payment.payment_amount,
+    #             "date": latest_payment.paid_date,
+    #             "method": latest_payment.get_payment_method_display()  # Красивое название метода
+    #         }
+    #     return "Нет платежей"
