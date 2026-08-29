c664590b11cea7a73d9a4d384ce9c4b0e92ec011
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-08-29 22:51:56 +0300

    created paginators for the course and lesson view classes

---
+++ lms/paginations.py
@@ -0,0 +1,15 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class CoursePageNumberPagination(PageNumberPagination):
+    """Класс пагинатор для разбития данных получения курсов на странице"""
+    page_size = 5 # Количество элементов на странице
+    page_size_query_param = "page_size" # Параметр запроса для указания количества элементов на странице
+    max_page_size = 10 # Максимальное количество элементов на странице
+
+
+class LessonPageNumberPagination(PageNumberPagination):
+    """Класс пагинатор для разбития данных получения уроков на страницы"""
+    page_size = 10 # Количество элементов на странице
+    page_size_query_param = "page_size" # Параметр запроса для указания количества элементов на странице
+    max_page_size = 20 # Максимальное количество элементов на странице
